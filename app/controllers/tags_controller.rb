331e6b473cf1af49ba7a7b2db7023de0712540a5
SamJamCul <samueljamesculley@gmail.com>
2018-03-05 15:38:36 +0000

    started working on deleting tags, hit issues with removing the taggings and foreign keying

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,6 +4,15 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@Tag.name}' Eradicated!"
+
+    redirect_to tags_path
+  end
+
   def show
     @tag = Tag.find(params[:id])
   end
